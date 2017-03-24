d27951ffef90daa925384596563c3eda15911907
qihaiyang <784311112@qq.com>
2017-03-24 16:35:21 +0800

    branch

--- src/main/java/tk/mybatis/springboot/GirlApplication.java
+++ src/main/java/tk/mybatis/springboot/GirlApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ImportResource;
 public class GirlApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GirlApplication.class, args);
 	}
 }
